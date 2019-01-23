51cd38385f0122026dff12cc051bf9f375a221af
Orangecat32 <Orangecat32@gmail.com>
2019-01-22 20:12:31 -0600

    add server

---
+++ server/app.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+const http = require('http');
+const websocket = require('ws');
+const socketIO = require('socket.io');
+const utils = require('./utils');
+const cors = require('cors');
+
+const portfolio = require('./public/mock/spx-2019-01-02.json');
+
+const port = process.env.PORT || 4000; // html page port
+const wsport = 3002; // websocket port
+const sioport = 8005; // socket-io port
+
+const app = express();
+const server = http.createServer(app);
+app.use(morgan('tiny'));
+app.use(cors());
+app.options('*', cors()); // For internal production use, whitelist local domain
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+
+
+// socket.io related  ----------------------------------------------------------------------
+
+const socketioServer = socketIO(server);
+
+socketioServer.listen(sioport);
+console.log('sio listening on port ', sioport);
+
+socketioServer.on('connection', (client) => {
+  client.on('subscribe', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
+});
+
+// websocket related  -----------------------------------------------------------------------------
+
+const websocketServer = new websocket.Server({ server });
+
+server.listen(wsport, () => { // note that the http server is used here, unlike socketio
+  debug(`websocket listening on port ${chalk.green(wsport)}`);
+  console.log(`Websocket server started on port ${wsport}`);
+});
+
+
+// when a websocket connection is established
+websocketServer.on('connection', (webSocketClient) => {
+  // send ack to the new client
+  console.log(`Websocket connection ${'ok'}`);
+  webSocketClient.send('{ "connection" : "ok"}');
+
+  webSocketClient.on('message', (msg) => {
+    console.log('incoming message:', msg);
+    const m = JSON.parse(msg);
+    console.log('msg: ', m);
+    if (m.command === 'subscribe') {
+      const interval = m.interval > 0 ? m.interval : 1000;
+      console.log('subscribe interval ', interval);
+      let tickData;
+      const updates = setInterval(() => {
+        try {
+          tickData = utils.buildTickUpdate(m, tickData);
+          webSocketClient.send(JSON.stringify(tickData));
+        } catch (err) {
+          console.log('err in set interval');
+          clearInterval(updates);
+        }
+      }, interval);
+    }
+  });
+});
+
+// --------------------------------------------------------------------------------
+//  Reference data
+
+//  parse refdata
+
+const sectors = portfolio.reduce((acc, p) => {
+  if (acc.indexOf(p.sector) === -1) {
+    acc.push(p.sector);
+  }
+  return acc;
+}, []);
+
+app.listen(port, () => { debug(`express listening on port ${chalk.green(port)}`); });
+
+app.get('/spx', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/mock/', '/spx-2019-01-02.json'));
+});
+
+app.get('/portfolio/:id', (req, res) => {
+  // convert + to space and make lowercase
+  const sectorName = req.params.id.replace(/\+/g, ' ').toLowerCase();
+  const names = portfolio.reduce((acc, p) => {
+    if (sectorName === p.sector.toLowerCase()) {
+      acc.push(p);
+    }
+    return acc;
+  }, []);
+
+  res.send(JSON.stringify(names));
+});
+
+
+app.get('/portfolio', (req, res) => {
+  res.send(JSON.stringify(portfolio));
+});
+
+
+app.get('/sectors', (req, res) => {
+  res.send(JSON.stringify(sectors));
+  // res.sendFile(path.join(__dirname, './public/mock/', '/sectors.json'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/', '/index.html'));
+});
+
---
+++ server/utils.js
@@ -0,0 +1,48 @@
+const sectors = require('./public/mock/sectors.json');
+const allTickers = require('./public/mock/spx-2019-01-02.json');
+
+const randomSign = () => (Math.floor(Math.random() * 2) % 2 ? 1 : -1);
+
+const initTickers = (m) => {
+  console.log('initTickData');
+
+  const sector = m && m.sector
+    ? m.sector
+    : sectors && sectors.length > 0
+      ? sectors[0] : 'Energy';
+
+  //  remove duplicates
+  const ts = Object.values(allTickers.reduce((acc, t) => Object.assign(acc, { [t.symbol]: t }), {}));
+
+  return {
+    sector,
+    timer: new Date(),
+    tickers: ts.filter(t => t.sector === sector)
+      .map(t => ({ ...t, last: t.close, increment: t.close * t.volPct50d / 1000, pc: 0 }))
+  };
+};
+
+const incrementTicker = (m, t) => {
+  const last = randomSign() * t.increment + t.last;
+  const pc = (last - t.close) / t.close;
+  const bid = last - 0.01;
+  const ask = last + 0.01;
+
+  return Object.assign(t, { last, pc, bid, ask });
+};
+
+
+const incrementTickers = (m, td) => {
+  return Object.assign(td, { timer: new Date(), tickers: (td.tickers || []).map(t => incrementTicker(m, t)) });
+};
+
+const buildTickUpdate = (m, td) => {
+  const tickData = td ? incrementTickers(m, td) : initTickers(m);
+  return Object.assign(tickData, { timer: new Date() });
+};
+
+
+module.exports = {
+  buildTickUpdate
+};
+
